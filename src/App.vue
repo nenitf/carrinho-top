1d24c0be66bc921718496702243430cae1869efe
Felipe Silva <felipe.dacs@gmail.com>
2018-08-04 14:42:22 -0300

    add transição

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
   <div id="app">
     <painel :produtos='produtos' :precoTotal='precoTotal'></painel>
     <itemNovo @novo='novo'></itemNovo>
-    <p class="help has-text-black-ter">Clique duas vezes no item desejado para excluí-lo</p>
+    <p class="help has-text-black-ter">Clique duas vezes no item para excluí-lo</p>
+
     <ul class="lista-produtos">
-      <li v-for='(produto, i) in produtos' :key='i' @dblclick='apaga(i)'>{{produto.nome}}; <span class="valor-total has-text-black-ter">R$ {{produto.precoTotal}}</span></li>
+      <transition-group name="fade">
+      <li v-for='(produto, i) in produtos' :key='i' @dblclick='apaga(i)' v-bind:key="produto">{{produto.nome}}({{produto.qtd}}) de R$ {{produto.preco}}; <span class="valor-total has-text-black-ter">R$ {{produto.precoTotal}}</span></li>
+      </transition-group>
     </ul>
+
   </div>
 </template>
 
@@ -58,6 +62,13 @@ export default {
   margin-top: 1rem;
   margin-left: 1rem !important;
   background-color: gray;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+.fade-enter, .fade-leave-active {
+  opacity: 0
 }
 
 .valor-total{
--- src/components/painel.vue
+++ src/components/painel.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div class="notification is-info">
     <h1 class="title is-5">Carrinho Top 🛒 🔝</h1>
-    <h1 class="title is-5">Total: {{precoTotal}}</h1>
+    <h1 class="title is-5">Total de <span class="has-text-black-ter">R$ {{precoTotal}}</span></h1>
   </div>
 
 </template>
